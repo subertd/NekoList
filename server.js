0654e2404126db78c62f52bdd3871fddf4f0e555
Donald Subert <helo478@gmail.com>
2015-06-04 01:34:43 -0700

    added require body parse

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ console.log('Server running at ' + server_ip_address + ":" + server_port);
 var express = require('express');
 var app = express();
 
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+//var lists = require('routes/')
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
