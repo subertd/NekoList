9380db06a0bfeaacdab5983dc0414d757abb0f51
Donald Subert <helo478@gmail.com>
2015-06-04 01:25:17 -0700

    Added express based on how-to guide

--- server.js
+++ server.js
@@ -11,16 +11,26 @@ http.createServer(function (req, res) {
 console.log('Server running at ' + server_ip_address + ":" + server_port);
   */
 
+//var http = require('http');
+//var server = http.createServer(app);
+
 
 var express = require('express');
 var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get("/", function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({
     test:"test test test"
   }));
+});
+
+var server = app.listen(server_port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
 });
